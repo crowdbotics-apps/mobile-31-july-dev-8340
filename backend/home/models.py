4f3bd03a29f17c5feacdd64f64e1bdcb34824ae9
Crowdbotics <team@crowdbotics.com>
2020-07-31 08:25:53 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -31,3 +31,8 @@ class HomePage(models.Model):
     @property
     def field(self):
         return "body"
+
+
+class Fjhgkjhkj(models.Model):
+    "Generated Model"
+    ghfhgfhf = models.BigIntegerField()
